dc0022666165763129fbe66e9b5ccdd21cd78487
amarazeez1992-eng <amarazeez1992@gmail.com>
2026-09-09 11:16:55 +0300

    Finalize compile-safe Aurora theme wrapper

--- app/src/main/java/com/personal/gridbot/amaros/design/AmarAuroraTheme.kt
+++ app/src/main/java/com/personal/gridbot/amaros/design/AmarAuroraTheme.kt
@@ -2,7 +2,6 @@ package com.personal.gridbot.amaros.design
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -10,8 +9,5 @@ fun AmarAuroraTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    MaterialTheme(
-        colorScheme = darkColorScheme(),
-        content = content
-    )
+    MaterialTheme(content = content)
 }
