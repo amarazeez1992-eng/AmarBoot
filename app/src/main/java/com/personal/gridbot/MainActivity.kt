12732d326dc288578a2ae9d7cbb45ade39f344ab
amarazeez1992-eng <amarazeez1992@gmail.com>
2026-09-09 16:49:46 +0300

    UI: make approved Arabic home the Android entry screen

--- app/src/main/java/com/personal/gridbot/MainActivity.kt
+++ app/src/main/java/com/personal/gridbot/MainActivity.kt
@@ -3,9 +3,7 @@ package com.personal.gridbot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Surface
-import com.personal.gridbot.amaros.core.AmarAppState
-import com.personal.gridbot.amaros.shell.AmarAuroraShell
+import com.personal.gridbot.ui.amarapproved.AmarApprovedHomeScreen
 import com.personal.gridbot.ui.theme.AmarTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,12 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AmarTheme {
-                Surface {
-                    AmarAuroraShell(
-                        initialState = AmarAppState(),
-                        onOpenLegacyGrid = { }
-                    )
-                }
+                AmarApprovedHomeScreen()
             }
         }
     }
