699f6351b5d121094dec2803ee39185eb5a65615
amarazeez1992-eng <amarazeez1992@gmail.com>
2026-09-10 16:50:45 +0300

    Remove unused legacy grid callback

--- app/src/main/java/com/personal/gridbot/amaros/shell/AmarAppShell.kt
+++ app/src/main/java/com/personal/gridbot/amaros/shell/AmarAppShell.kt
@@ -2,15 +2,13 @@ package com.personal.gridbot.amaros.shell
 
 import androidx.compose.runtime.Composable
 import com.personal.gridbot.amaros.core.AmarAppState
-import com.personal.gridbot.amaros.navigation.AmarRoom
 import com.personal.gridbot.amaros.navigation.AmarRoomHostScreen
 
 /** Compatibility entry point. The approved living home is hosted by MainActivity. */
 @Composable
-fun AmarAppShell(initialState: AmarAppState = AmarAppState(), onOpenLegacyGrid: () -> Unit = {}) {
+fun AmarAppShell(initialState: AmarAppState = AmarAppState()) {
     AmarRoomHostScreen(
         room = initialState.selectedRoom,
-        onBackHome = {},
-        onOpenLegacyGrid = onOpenLegacyGrid
+        onBackHome = {}
     )
 }
