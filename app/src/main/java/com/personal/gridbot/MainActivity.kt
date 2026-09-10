4d26ed0eced39576404cb534a298e48493d917e9
amarazeez1992-eng <amarazeez1992@gmail.com>
2026-09-10 12:08:07 +0300

    Connect MainActivity to interactive AMAR app shell

--- app/src/main/java/com/personal/gridbot/MainActivity.kt
+++ app/src/main/java/com/personal/gridbot/MainActivity.kt
@@ -1,24 +1,29 @@
 package com.personal.gridbot
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxSize
+import com.personal.gridbot.amaros.shell.AmarAppShell
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val webView = WebView(this).apply {
-            webViewClient = WebViewClient()
-            settings.javaScriptEnabled = true
-            settings.domStorageEnabled = true
-            settings.cacheMode = WebSettings.LOAD_DEFAULT
-            setBackgroundColor(android.graphics.Color.TRANSPARENT)
-            loadUrl("file:///android_asset/amar_reference.html")
+        setContent {
+            AmarRoot()
         }
-        setContentView(webView)
+    }
+}
+
+@Composable
+private fun AmarRoot() {
+    MaterialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            AmarAppShell()
+        }
     }
 }
