e25bf3e6dcf577a251fe103c770fdf4c84d4f0c6
amarazeez1992-eng <amarazeez1992@gmail.com>
2026-09-10 12:18:07 +0300

    Restore approved white Aurora interface as app entry

--- app/src/main/java/com/personal/gridbot/MainActivity.kt
+++ app/src/main/java/com/personal/gridbot/MainActivity.kt
@@ -3,27 +3,13 @@ package com.personal.gridbot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.fillMaxSize
-import com.personal.gridbot.amaros.shell.AmarAppShell
+import com.personal.gridbot.amaros.shell.AmarAuroraShell
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AmarRoot()
-        }
-    }
-}
-
-@Composable
-private fun AmarRoot() {
-    MaterialTheme {
-        Surface(modifier = Modifier.fillMaxSize()) {
-            AmarAppShell()
+            AmarAuroraShell()
         }
     }
 }
