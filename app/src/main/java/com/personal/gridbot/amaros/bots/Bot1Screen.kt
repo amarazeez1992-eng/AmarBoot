3e287ac5f1bc3ddd33a0235d3f763f5486f47201
amarazeez1992-eng <amarazeez1992@gmail.com>
2026-09-10 19:56:40 +0300

    Activate premium V1 BOT 1 workspace

---
+++ app/src/main/java/com/personal/gridbot/amaros/bots/Bot1Screen.kt
@@ -0,0 +1,9 @@
+package com.personal.gridbot.amaros.bots
+
+import androidx.compose.runtime.Composable
+
+/** BOT LAB entry point. V1 now uses the premium BOT 1 workspace. */
+@Composable
+fun BotLabScreen() {
+    Bot1PremiumScreen()
+}
