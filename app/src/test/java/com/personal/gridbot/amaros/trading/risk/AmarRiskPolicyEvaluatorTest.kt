c9786b7be9be8d74eb27eb70faa623b8443bdc85
amarazeez1992-eng <amarazeez1992@gmail.com>
2026-09-13 13:54:07 +0300

    fix: correct risk test JUnit imports

--- app/src/test/java/com/personal/gridbot/amaros/trading/risk/AmarRiskPolicyEvaluatorTest.kt
+++ app/src/test/java/com/personal/gridbot/amaros/trading/risk/AmarRiskPolicyEvaluatorTest.kt
@@ -1,7 +1,7 @@
 package com.personal.gridbot.amaros.trading.risk
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class AmarRiskPolicyEvaluatorTest {
     @Test
