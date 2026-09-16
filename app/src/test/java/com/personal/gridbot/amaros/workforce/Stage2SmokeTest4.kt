c93745ad1d375b8ff2881bcd13b3574961c887a1
amarazeez1992-eng <amarazeez1992@gmail.com>
2026-09-17 01:15:13 +0300

    test: verify Stage 2 Agent core construction

---
+++ app/src/test/java/com/personal/gridbot/amaros/workforce/Stage2SmokeTest4.kt
@@ -0,0 +1,11 @@
+package com.personal.gridbot.amaros.workforce
+
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+
+class Stage2SmokeTest4 {
+    @Test
+    fun stageTwo_core_can_be_created() {
+        assertNotNull(AmarAgentCore())
+    }
+}
