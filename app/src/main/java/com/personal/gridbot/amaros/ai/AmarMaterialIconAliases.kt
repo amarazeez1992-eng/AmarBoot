c7bb4b03c104e753fea4bbbf8f9eeb333e7784ed
amarazeez1992-eng <amarazeez1992@gmail.com>
2026-09-16 17:46:33 +0300

    fix(ui): bind icon aliases as Icons.Filled extensions

--- app/src/main/java/com/personal/gridbot/amaros/ai/AmarMaterialIconAliases.kt
+++ app/src/main/java/com/personal/gridbot/amaros/ai/AmarMaterialIconAliases.kt
@@ -3,10 +3,10 @@ package androidx.compose.material.icons.filled
 import androidx.compose.material.icons.Icons
 import androidx.compose.ui.graphics.vector.ImageVector
 
-val CameraAlt: ImageVector get() = Icons.Filled.Add
-val Description: ImageVector get() = Icons.Filled.Add
-val Folder: ImageVector get() = Icons.Filled.Add
-val Mic: ImageVector get() = Icons.Filled.Add
-val Monitor: ImageVector get() = Icons.Filled.Add
-val Stop: ImageVector get() = Icons.Filled.Close
-val VolumeUp: ImageVector get() = Icons.Filled.Add
+val Icons.Filled.CameraAlt: ImageVector get() = Icons.Filled.Add
+val Icons.Filled.Description: ImageVector get() = Icons.Filled.Add
+val Icons.Filled.Folder: ImageVector get() = Icons.Filled.Add
+val Icons.Filled.Mic: ImageVector get() = Icons.Filled.Add
+val Icons.Filled.Monitor: ImageVector get() = Icons.Filled.Add
+val Icons.Filled.Stop: ImageVector get() = Icons.Filled.Close
+val Icons.Filled.VolumeUp: ImageVector get() = Icons.Filled.Add
