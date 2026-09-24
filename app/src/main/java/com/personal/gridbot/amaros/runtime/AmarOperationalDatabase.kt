e6d2b71e8e8b5272cea69454cad2d6c960fb1d89
amarazeez1992-eng <amarazeez1992@gmail.com>
2026-09-24 20:57:35 +0300

    Revert out-of-scope Item 1 storage change

--- app/src/main/java/com/personal/gridbot/amaros/runtime/AmarOperationalDatabase.kt
+++ app/src/main/java/com/personal/gridbot/amaros/runtime/AmarOperationalDatabase.kt
@@ -69,10 +69,6 @@ data class AmarRuntimeCommandRecord(
     val error: String? = null
 )
 
-interface AmarStorage {
-    fun dao(): AmarOperationalDao
-}
-
 @Dao
 interface AmarOperationalDao {
     @Query("SELECT * FROM amar_bot_runtime ORDER BY botNumber ASC")
@@ -120,8 +116,8 @@ interface AmarOperationalDao {
     version = 1,
     exportSchema = false
 )
-abstract class AmarOperationalDatabase : RoomDatabase(), AmarStorage {
-    override abstract fun dao(): AmarOperationalDao
+abstract class AmarOperationalDatabase : RoomDatabase() {
+    abstract fun dao(): AmarOperationalDao
 
     companion object {
         @Volatile private var instance: AmarOperationalDatabase? = null
